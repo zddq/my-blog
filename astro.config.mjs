ab8deafdc579601e2c41e914903334d96d2d5cd7
ZDDQ <minimalism_zddq@163.com>
2024-08-09 16:08:47 +0800

    🎨 移除多余目录及文件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,20 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import path from "path";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+console.log(path.resolve("src"));
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	root: ".",
+	base: ".",
+	site: "https://example.com",
+	trailingSlash: "ignore",
 	integrations: [mdx(), sitemap()],
+	vite: {
+		resolve: {
+			"@": path.resolve("src/"),
+		},
+	},
 });
---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as HeadSeo } from "./HeadSeo.astro";
+export { default as Header } from "./Header.astro";
+export { default as Footer } from "./Footer.astro";
+export { default as Layout } from "./layouts/index.astro";
